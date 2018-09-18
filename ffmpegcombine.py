138232ad073d24065c86f5017b3587f65f9f70b5
SandyHao <43019863+SandyHao@users.noreply.github.com>
2018-09-18 16:49:38 -0400

    for different size pictures
    this vision allow the pictures to be variety of sizes

--- ffmpegcombine.py
+++ ffmpegcombine.py
@@ -1,20 +1,6 @@
-#应用ffmpeg
-import os,numpy,sys
-#将图片化成同一尺寸
-os.system("ffmpeg -f image2 -framerate 12 -i foo-%03d.jpeg -s WxH foo.avi")
-
-
-
-#应用openCV
-import cv2
-import glob
-
-fps = 10
-
-fourcc = cv2.VideoWriter_fourcc(*'MJPG')
-videoWriter = cv2.VideoWriter('saveVideo.avi', fourcc, fps, (480, 360))  # the last parameters are size of the picture
-imgs = glob.glob('D:/experiment/ViBE/image_to_video/val/*.png')
-for imgname in imgs:
-    frame = cv2.imread(imgname)
-    videoWriter.write(frame)
-videoWriter.release()
+import os,sys,ffmpeg
+os.system("ffmpeg -r 1 -f image2 -i %3d.jpg -s 1200x800 models.mp4")
+#"-r 1" one frames per second
+#"-f image2" the format 
+#"%3d" ex.003
+#"-s 1200x800" size of the video 
