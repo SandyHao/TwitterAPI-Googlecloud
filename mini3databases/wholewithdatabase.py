66992a75fa79a3209d7ae1dc17e936f1d5738b63
SandyHao <43019863+SandyHao@users.noreply.github.com>
2018-11-30 17:28:27 -0500

    the newest one

--- mini3databases/wholewithdatabase.py
+++ mini3databases/wholewithdatabase.py
@@ -2,6 +2,7 @@
 # encoding: utf-8
 # author SichunHao
 #export GOOGLE_APPLICATION_CREDENTIALS="/home/ece-student/601_mini1/My First Project-ab2764d710b6.json"
+import collections
 import tweepy #https://github.com/tweepy/tweepy
 import json
 import os,sys,ffmpeg
@@ -12,10 +13,10 @@ import datetime
 from PIL import Image
 from PIL import ImageDraw
 from PIL import ImageFont
-consumer_key = ""              #"Enter the consumer_key"
+consumer_key = ""                                       #"Enter the consumer_key"
 consumer_secret = ""           #"Enter the consumer_secret"
 access_key = ""                #"Enter the access_key"
-access_secret = ""             #"Enter the access_secret"
+access_secret = ""                  #"Enter the access_secret"
 from skimage import io
 # Imports the Google Cloud client library
 from google.cloud import vision
@@ -76,86 +77,102 @@ if __name__ == '__main__':
 	import io
 	# Instantiates a client
 	client = vision.ImageAnnotatorClient()
-	for i in range(1,tweetsnumber):
-	# The name of the image file to annotate
-		file_name = os.path.join(os.path.dirname(__file__),'%03d.jpg'%(i))
-		font=ImageFont.truetype("C:\Windows\Fonts\Arial.ttf", 30)#font
-		im=Image.open(file_name)#drawopen
-		# Loads the image into memory
-		with io.open(file_name, 'rb') as image_file:
-			content = image_file.read()
-
-		image = types.Image(content=content)
-
-		# Performs label detection on the image file
-		response = client.label_detection(image=image)
-		labels = response.label_annotations
-
-		print('image%dlabels:'%i)
-		j=1
-		for label in labels:
-			j=j+1
-			print(label.description)
-			print(label.score)
-			draw=ImageDraw.Draw(im)
-			loca=j*30
-			draw.text((50,loca),label.description,(255,0,0),font=font)
-			draw=ImageDraw.Draw(im)
-			im.save('%03d.jpg'%(i))
-
-
-	#parameters need to be stored
-	for i in len(labels):
-		if(i==0):
-			keyword1=labels[0]
-		elif(i==1):
-			keyword2=labels[1]
-		elif(i==2):
-			keyword3=labels[2]
-		elif(i==3):
-			keyword4=labels[3]
-		elif(i==4):
-			keyword5=labels[4]	
-		elif(i==5):
-			keyword6=labels[5]
-	
-		#mongodb
-		myclient = pymongo.MongoClient("mongodb://localhost:27017/")
-		mydb = myclient["mini3mongo"]
-		mycol = mydb["tweetsinquiry"]
-		record = {"time":datetime.datetime.now(),"username":username,"tweetname":tweetname,"tweetsnumber":tweetsnumber,
-				"keyword1":keyword1,"keyword2":keyword2,"keyword3":keyword3,"keyword4":keyword4,
-				"keyword5":keyword5,"keyword6":keyword6}
-		x=mycol.insert_one(record)
-		myquery = input("please input the keywords you want to search in database")
-		mydoc = mycol.find(myquery)
-		for x in mydoc:
-			  print(x)
-		#mysql  
-		db= pymysql.connect("localhost","root",passwordhere,"601mini3")  
-		cursor = db.cursor()  
-		sql="INSERT INTO tweetsinquiry value(%s,%s,%s,%s,%s,%s,%s,%s,%s)"
+	for i in range(1,2*tweetsnumber):
+		count=0
 		try:
-			cursor.execute(sql,[username,tweetname,tweetsnumber,keyword1,keyword2,keyword3,keyword4,keyword5,keyword6])
-		# submit to database
-			db.commit()
-		except:
-			db.rollback()
-		try:
-			results = cursor.fetchall(myquery)
-			for row in results:
-				print(row)
-		except:
-			print("unable to fetch")
+			# The name of the image file to annotate
+			file_name = os.path.join(os.path.dirname(__file__),'%03d.jpg'%(i))
+			font=ImageFont.truetype("C:\Windows\Fonts\Arial.ttf", 30)#font
+			im=Image.open(file_name)#drawopen
+			# Loads the image into memory
+			with io.open(file_name, 'rb') as image_file:
+				content = image_file.read()
 
-		#------------------------------------------------------------------------
-		try:
-			highest = select 列名, count(*) from 表名 group by 列名 order by count(*) desc
-			for row in highest:
-				print(row)
+			image = types.Image(content=content)
+
+			# Performs label detection on the image file
+			response = client.label_detection(image=image)
+			labels = response.label_annotations
+
+			print('image%dlabels:'%i)
+			j=1
+			for label in labels:
+				j=j+1
+				print(label.description)
+				print(label.score)
+				draw=ImageDraw.Draw(im)
+				loca=j*30
+				draw.text((50,loca),label.description,(255,0,0),font=font)
+				draw=ImageDraw.Draw(im)
+				im.save('%03d.jpg'%(i))
+
+			count=count+1
+			#parameters need to be stored
+			for i in len(labels):
+				if(i==0):
+					keyword1=labels[0]
+				elif(i==1):
+					keyword2=labels[1]
+				elif(i==2):
+					keyword3=labels[2]
+				elif(i==3):
+					keyword4=labels[3]
+				elif(i==4):
+					keyword5=labels[4]	
+				elif(i==5):
+					keyword6=labels[5]
+			
+				#mongodb
+				myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+				mydb = myclient["mini3mongo"]
+				mycol = mydb["tweetsinquiry"]
+				record = {"time":datetime.datetime.now(),"username":username,"tweetname":tweetname,"tweetsnumber":tweetsnumber,
+						"keyword1":keyword1,"keyword2":keyword2,"keyword3":keyword3,"keyword4":keyword4,
+						"keyword5":keyword5,"keyword6":keyword6}
+				x=mycol.insert_one(record)
+				#mysql  
+				db= pymysql.connect("localhost","root",password here,"601mini3")  
+				cursor = db.cursor()  
+				sql="INSERT INTO tweetsinquiry value(%s,%s,%s,%s,%s,%s,%s,%s,%s)"
+				try:
+					cursor.execute(sql,[username,tweetname,tweetsnumber,keyword1,keyword2,keyword3,keyword4,keyword5,keyword6])
+				# submit to database
+					db.commit()
+				except:
+					db.rollback()
+				db.close()
+			# find the number of images or the keywords exist most
+			filename = 'storedatabase1.txt'
+			with open(filename,'a') as f: 
+				# 'a': append,add data to original file
+				f.write("username"+username+"\n")
+				f.write("tweetname"+tweetname+"\n")
+				f.write("tweetsnumber"+tweetsnumber+"\n")
+				f.write("imagenumber"+count+"\n")
+				f.write(keyword1+"\n")
+				f.write(keyword2+"\n")
+				f.write(keyword3+"\n")
+				f.write(keyword4+"\n")
+				f.write(keyword5+"\n")
+				f.write(keyword6+"\n")
+			'''#users information
+			filename = 'storedatabase2.txt'			
+			with open(filename,'a') as f: 
+				# 'a': append,add data to original file
+				f.write("username"+username+"\n")
+				f.write("tweetname"+tweetname+"\n")
+				f.write("tweetsnumber"+tweetsnumber+"\n")
+				f.write("imagenumber"+count+"\n")
+			'''
 		except:
-			print("unable to fetch")
-		#------------------------------------------------------------------------
-		db.close()
+			pass
+
+
 
 os.system("ffmpeg -r 1 -f image2 -i %3d.jpg -s 1200x800 models.mp4")
+#overal number & keyword
+with open('storedatabase1.txt') as file1:
+	str1=file1.read().split()
+	print ("orinial dataset:\n %s"% str1)
+	print ("\ntimes for each word：\n %s" % collections.Counter(str1))
+	#print collections.Counter(str1)['the word we want to search']
